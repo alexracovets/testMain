ac4c2532047be9dd4d45e9c72d9c8ea428f81a60
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-22 22:29:33 +0300

    test

--- src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
+++ src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
@@ -12,8 +12,13 @@ const Voxel = React.memo(({ index }) => {
     const instanceRef = useRef();
     const activePage = useStore(state => state.activePage);
     const [currentPosition, setCurrentPosition] = useState(null);
-    const tlNoramal = gsap.timeline({})
-    const tlPasive = gsap.timeline({ ease: "expoScale(0.5,7,none)"});
+    const tlNoramal = gsap.timeline({
+        ease: "expoScale(0.5,7,none)",
+    })
+    const tlPasive = gsap.timeline({
+        ease: "expoScale(0.5,7,none)",
+        repeat: -1,
+    });
 
     useEffect(() => {
         const instance = instanceRef.current;
@@ -27,14 +32,12 @@ const Voxel = React.memo(({ index }) => {
             x: position[0],
             y: position[1],
             z: position[2],
-            ease: "expoScale(0.5,7,none)",
             duration: 1,
         })
         tlNoramal.to(instance.scale, {
             x: sizes[activePage],
             y: sizes[activePage],
             z: sizes[activePage],
-            ease: "expoScale(0.5,7,none)",
             duration: 1,
             onComplete: () => setCurrentPosition(position)
         }, "<");
@@ -44,17 +47,33 @@ const Voxel = React.memo(({ index }) => {
     useEffect(() => {
         const instance = instanceRef.current;
         if (currentPosition) {
+            const randomX = Math.random();
+            const randomY = Math.random();
+            const randomZ = Math.random();
+            randomX < 0.3 && tlPasive.to(instance.position, {
+                x: currentPosition[0] + (Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2),
+                delay: Math.random(),
+                duration: 1 + Math.random(),
+            });
+            randomY < 0.3 && tlPasive.to(instance.position, {
+                y: currentPosition[1] + (Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2),
+                delay: Math.random(),
+                duration: 1 + Math.random(),
+            });
+            randomZ < 0.3 && tlPasive.to(instance.position, {
+                z: currentPosition[2] + (Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2),
+                delay: Math.random(),
+                duration: 1 + Math.random(),
+            });
             tlPasive.to(instance.position, {
                 x: currentPosition[0],
-                y: currentPosition[1] + (Math.random() - 0.5),
+                y: currentPosition[1],
                 z: currentPosition[2],
-                duration: 2,
-                repeat: -1,
-                yoyo: true
+                duration: 1.5,
             });
         }
         return () => tlPasive.kill()
-    }, [currentPosition])
+    }, [currentPosition, activePage])
     return (
         <Instance
             ref={instanceRef}
