87d6b4d5b1f154c63d8f0987a3aa055a03cc111e
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-22 20:40:12 +0300

    test

--- src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
+++ src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
@@ -11,31 +11,57 @@ const sizes = [0.15, 0.2267, 0.28, 0.316];
 const Voxel = React.memo(({ index }) => {
     const instanceRef = useRef();
     const activePage = useStore(state => state.activePage);
-    const [active, setActive] = useState(false);
+    const [currentPosition, setCurrentPosition] = useState(null);
+    const tlNoramal = gsap.timeline({})
+    const tlPasive = gsap.timeline({});
 
     useEffect(() => {
         const instance = instanceRef.current;
-        const tl = gsap.timeline({
-            onStart: () => setActive(true),
-            onComplete: () => setActive(false)
-        })
-        tl.to(instance.position, {
-            x: voxelsData[activePage][index * 3],
-            y: voxelsData[activePage][index * 3 + 1],
-            z: voxelsData[activePage][index * 3 + 2],
+        const position = [
+            voxelsData[activePage][index * 3],
+            voxelsData[activePage][index * 3 + 1],
+            voxelsData[activePage][index * 3 + 2]
+        ]
+        setCurrentPosition(null)
+        tlNoramal.to(instance.position, {
+            x: position[0],
+            y: position[1],
+            z: position[2],
             ease: "expoScale(0.5,7,none)",
             duration: .5,
         })
-        tl.to(instance.scale, {
+        tlNoramal.to(instance.scale, {
             x: sizes[activePage],
             y: sizes[activePage],
             z: sizes[activePage],
             ease: "expoScale(0.5,7,none)",
             duration: .5,
+            onComplete: () => setCurrentPosition(position)
         }, "<");
-
+        return () => tlNoramal.kill()
     }, [activePage, index])
-    
+
+    useEffect(() => {
+        const instance = instanceRef.current;
+        if (currentPosition) {
+            tlPasive.to(instance.position, {
+                x: currentPosition[0],
+                y: currentPosition[1] + (Math.random() - 0.5),
+                z: currentPosition[2],
+                ease: "expoScale(0.5,7,none)",
+                duration: 2,
+            });
+            tlPasive.to(instance.position, {
+                x: currentPosition[0],
+                y: currentPosition[1],
+                z: currentPosition[2],
+                ease: "expoScale(0.5,7,none)",
+                duration: 2,
+                delay: 2
+            }, "<");
+        }
+        return () => tlPasive.kill()
+    }, [currentPosition])
     return (
         <Instance
             ref={instanceRef}
