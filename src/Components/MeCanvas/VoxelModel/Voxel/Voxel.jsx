3a2f1070c0c230954348ab1c5db888dbaa7e2b57
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-25 21:10:05 +0300

    change

--- src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
+++ src/Components/MeCanvas/VoxelModel/Voxel/Voxel.jsx
@@ -34,65 +34,133 @@ const Voxel = React.memo(({ index }) => {
 
     useGSAP(() => {
         const instance = instanceRef.current;
-
+        const instance2 = wrapperWRef.current;
         if (activePage !== -1) {
-            const position = [voxelsData[activePage][index * 3], voxelsData[activePage][index * 3 + 1], voxelsData[activePage][index * 3 + 2]];
+            const position = [
+                voxelsData[activePage][index * 3],
+                voxelsData[activePage][index * 3 + 1],
+                voxelsData[activePage][index * 3 + 2]
+            ];
             const size = sizes[activePage];
-            const tl = gsap.timeline({ ease: "expoScale(0.5,7,none)" });
-
+            const tl = gsap.timeline({
+                ease: "expoScale(0.5,7,none)"
+            });
+            const tl2 = gsap.timeline({
+                ease: "expoScale(0.5,7,none)"
+            })
+            
             tl
-                .to(instance.position, { x: (Math.random() - 0.5) * 10, y: (Math.random() - 0.5) * 10, z: (Math.random() - 0.5) * 10, duration: .5 })
-                .to(instance.scale, { x: 0.01, y: 0.01, z: 0.01, duration: .5 }, "<")
-                .to(instance.position, { x: position[0], y: position[1], z: position[2], duration: 1 })
-                .to(instance.scale, { x: size, y: size, z: size, duration: 1 }, "<")
+                .to(instance.position, {
+                    x: (Math.random() - 0.5) * 10,
+                    y: (Math.random() - 0.5) * 10,
+                    z: (Math.random() - 0.5) * 10,
+                    duration: .5,
+                })
+                .to(instance.scale, {
+                    x: 0.01,
+                    y: 0.01,
+                    z: 0.01,
+                    duration: .5,
+                }, "<")
+                .to(instance.position, {
+                    x: position[0],
+                    y: position[1],
+                    z: position[2],
+                    duration: 1,
+                })
+                .to(instance.scale, {
+                    x: size,
+                    y: size,
+                    z: size,
+                    duration: 1
+                }, "<")
+
+            tl2
+                .to(instance2.position, {
+                    x: modelCoords[activePage].position[0],
+                    y: modelCoords[activePage].position[1],
+                    z: modelCoords[activePage].position[2],
+                    delay: .5,
+                    duration: 1,
+                })
+                .to(instance2.rotation, {
+                    x: modelCoords[activePage].rotation[0],
+                    y: modelCoords[activePage].rotation[1],
+                    z: modelCoords[activePage].rotation[2],
+                    duration: 1,
+                }, "<")
+
         } else {
-            const tl = gsap.timeline({ ease: "expoScale(0.5,7,none)" });
+            const tl = gsap.timeline({
+                ease: "expoScale(0.5,7,none)"
+            });
 
             tl
-                .to(instance.position, { x: (Math.random() - 0.5) * 20, y: (Math.random() - 0.5) * 20, z: (Math.random() - 0.5) * 20, duration: .5 })
-                .to(instance.scale, { x: 0.01, y: 0.01, z: 0.01, duration: .5 }, "<")
+                .to(instance.position, {
+                    x: (Math.random() - 0.5) * 10,
+                    y: (Math.random() - 0.5) * 10,
+                    z: (Math.random() - 0.5) * 10,
+                    duration: .5,
+                })
+                .to(instance.scale, {
+                    x: 0.01,
+                    y: 0.01,
+                    z: 0.01,
+                    duration: .5,
+                }, "<")
         }
 
     }, [activePage]);
 
     useGSAP(() => {
-        const instance = wrapperWRef.current;
-        const tl = gsap.timeline({ ease: "expoScale(0.5,7,none)" })
-        const rotation = [activePage !== -1 ? modelCoords[activePage].rotation[0] : 0, activePage !== -1 ? modelCoords[activePage].rotation[1] : 0, activePage !== -1 ? modelCoords[activePage].rotation[2] : 0]
-        const position = [activePage !== -1 ? modelCoords[activePage].position[0] : 0, activePage !== -1 ? modelCoords[activePage].position[1] : 0, activePage !== -1 ? modelCoords[activePage].position[2] : 0]
-        tl
-            .to(instance.position, { x: position[0], y: position[1], z: position[2], delay: .5, duration: 1 })
-            .to(instance.rotation, { x: rotation[0], y: rotation[1], z: rotation[2], duration: 1 }, "<")
-    }, [activePage]);
+        const step = 0.5;
+        const instance = wrapperRef.current;
+        const tlPasive = gsap.timeline({
+            ease: "expoScale(0.5,7,none)",
+            repeat: -1,
+        });
+        const randomX = Math.random();
+        const randomY = Math.random();
+        const randomZ = Math.random();
 
-    useGSAP(() => {
-        if (activePage !== -1) {
-            const instance = wrapperRef.current;
-            const tlPasive = gsap.timeline({ ease: "expoScale(0.5,7,none)", repeat: -1 });
-            const randomX = Math.random();
-            const randomY = Math.random();
-            const randomZ = Math.random();
+        const position = [
+            (Math.random() < 0.5 ? step / 2 : -step / 2),
+            (Math.random() < 0.5 ? step / 2 : -step / 2),
+            (Math.random() < 0.5 ? step / 2 : -step / 2)
+        ]
 
-            const position = [(Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2), (Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2), (Math.random() < 0.5 ? sizes[activePage] / 2 : -sizes[activePage] / 2)]
-
-            randomX < 0.3 && tlPasive.to(instance.position, { x: position[0], delay: Math.random(), duration: 1 + Math.random() });
-            randomY < 0.3 && tlPasive.to(instance.position, { y: position[1], delay: Math.random(), duration: 1 + Math.random() });
-            randomZ < 0.3 && tlPasive.to(instance.position, { z: position[2], delay: Math.random(), duration: 1 + Math.random() });
-            tlPasive.to(instance.position, { x: 0, y: 0, z: 0, duration: 1.5 });
-        }
+        randomX < 0.3 && tlPasive.to(instance.position, {
+            x: position[0],
+            delay: Math.random(),
+            duration: 1 + Math.random(),
+        });
+        randomY < 0.3 && tlPasive.to(instance.position, {
+            y: position[1],
+            delay: Math.random(),
+            duration: 1 + Math.random(),
+        });
+        randomZ < 0.3 && tlPasive.to(instance.position, {
+            z: position[2],
+            delay: Math.random(),
+            duration: 1 + Math.random(),
+        });
+        tlPasive.to(instance.position, {
+            x: 0,
+            y: 0,
+            z: 0,
+            duration: 1.5,
+        });
     }, [])
 
     return (
-        <>
-            <group ref={wrapperWRef} >
-                <group ref={wrapperRef}  >
-                    <Instance
-                        ref={instanceRef}
-                        scale={[0, 0, 0]}
-                    />
-                </group>
-            </group >
-        </>
+        <group ref={wrapperWRef}>
+            <group ref={wrapperRef}  >
+                <Instance
+                    ref={instanceRef}
+                    scale={[0, 0, 0]}
+                />
+            </group>
+        </group>
     );
 });
 Voxel.displayName = 'Voxel';
