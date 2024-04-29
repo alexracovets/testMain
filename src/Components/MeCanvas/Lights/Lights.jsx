230be317ae0cb507195f1f0c788141702cc5b553
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-29 18:11:58 +0300

    fixed style

--- src/Components/MeCanvas/Lights/Lights.jsx
+++ src/Components/MeCanvas/Lights/Lights.jsx
@@ -1,7 +1,7 @@
 export default function Lights() { 
     return (
         <>
-            <color attach={'background'} args={['#292929']} />
+            <color attach={'background'} args={['#000']} />
             <pointLight position={[-30, 0, -30]} power={10.1} />
             <ambientLight />
             <directionalLight />
