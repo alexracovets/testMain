47da519d3315508fe736b1bdc42bdf4f5db78f0b
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-05-15 13:32:22 +0300

    fix

--- src/Components/Fliper/Fliper.jsx
+++ src/Components/Fliper/Fliper.jsx
@@ -16,7 +16,7 @@ export default function Fliper() {
     useEffect(() => {
         setInterval(() => {
             setActiveSentences((prev) => (prev + 1) % sentences.length)
-        }, 1000);
+        }, 10000);
     }, []);
 
     return (
