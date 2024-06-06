605dd0d7c0266e13b1f3cce32ef0d208670e7734
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-06 08:47:20 +0300

    fixed days

--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -4,13 +4,32 @@ import linkedin from '/image/icons/social/linkedin.svg';
 
 import s from './Socials.module.scss';
 export default function Socials() {
-
+    const days = [
+        {
+            day: 'Mon - Fri:',
+            time: '10:00 AM - 6:00 PM'
+        },
+        {
+            day: 'Sat:',
+            time: '10:00 AM - 3:00 PM'
+        },
+        {
+            day: 'Sun:',
+            time: 'Day Off'
+        }
+    ]
     return (
         <>
-            <div className={s.work_days}>
-                Mon - Fri: 10:00 AM - 5:00 PM. <br />
-                Sat - 10:00 AM - 3:00 PM
-            </div>
+            <ul className={s.work_days}>
+                {days.map((item, idx) => {
+                    return (
+                        <li key={idx}>
+                            <span className={s.day}>{item.day}</span>
+                            <span className={s.work_time}>{item.time}</span>
+                        </li>
+                    )
+                })}
+            </ul>
             <div className={s.socials}>
                 <button className={s.socialsBtn}>
                     <img src={instagram} alt='Instagram' />
