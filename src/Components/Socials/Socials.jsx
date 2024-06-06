d583e7c37d7ac3227fcfcbc98e0d033b1aa270ab
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-06 08:52:56 +0300

    fixed days

--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -3,6 +3,7 @@ import facebook from '/image/icons/social/facebook.svg';
 import linkedin from '/image/icons/social/linkedin.svg';
 
 import s from './Socials.module.scss';
+import { Link } from 'react-router-dom';
 export default function Socials() {
     const days = [
         {
@@ -30,26 +31,32 @@ export default function Socials() {
                     )
                 })}
             </ul>
-            <div className={s.socials}>
-                <button className={s.socialsBtn}>
-                    <img src={instagram} alt='Instagram' />
-                    <span className={s.text}>
-                        Instagram
-                    </span>
-                </button>
-                <button className={s.socialsBtn}>
-                    <img src={facebook} alt="Facebook" />
-                    <span className={s.text}>
-                        Facebook
-                    </span>
-                </button>
-                <button className={s.socialsBtn}>
-                    <img src={linkedin} alt="LinkedIn" />
-                    <span className={s.text}>
-                        LinkedIn
-                    </span>
-                </button>
-            </div>
+            <ul className={s.socials}>
+                <li className={s.socialsBtn}>
+                    <Link target="_blank" to={"https://www.instagram.com/spotium360?igsh=MXMzcDJnaDR6ZDdmcw%3D%3D&utm_source=qr"}>
+                        <img src={instagram} alt='Instagram' />
+                        <span className={s.text}>
+                            Instagram
+                        </span>
+                    </Link>
+                </li>
+                <li className={s.socialsBtn}>
+                    <Link target="_blank" to={"https://www.facebook.com/share/gCgVnDrZshvm25W4/?mibextid=LQQJ4d"}>
+                        <img src={facebook} alt="Facebook" />
+                        <span className={s.text}>
+                            Facebook
+                        </span>
+                    </Link>
+                </li>
+                <li className={s.socialsBtn}>
+                    <Link target="_blank" to={"https://www.linkedin.com/company/spotium-360/"}>
+                        <img src={linkedin} alt="LinkedIn" />
+                        <span className={s.text}>
+                            LinkedIn
+                        </span>
+                    </Link>
+                </li>
+            </ul>
         </>
     )
 } 
