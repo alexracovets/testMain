7afbe64d4f07b51966587e8103a28a3d3b432014
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-14 12:17:33 +0300

    fixed style

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -17,7 +17,7 @@ export default function Loader() {
     const isLoaded = useLoader((state) => state.isLoaded);
     const setIsLoaded = useLoader((state) => state.setIsLoaded);
 
-    useEffect(() => { 
+    useEffect(() => {
         if (progress === '100') {
             setTimeout(() => {
                 setActiveBtn(true);
@@ -34,37 +34,38 @@ export default function Loader() {
 
     return (
         <AnimatePresence>
-            {!isLoaded ? <motion.div className={s.loader__wrapper}
-                initial={{ opacity: 1 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                exit={{ opacity: 0, scale: 2 }}
-            >
-                <div className={s.circle}>
-                    <div className={s.mask}></div>
-                    <div className={s.dot}></div>
-                </div>
-                <div className={s.loader__progress}>
-                    <CountUp end={progress} />%
-                </div>
-                <div className={activeBtn ? s.btn + ' ' + s.active : s.btn} onClick={() => setIsLoaded(true)}>
-                    <UI_Button text={'To Explore'} arrow />
-                </div>
-                <css-doodle
-                    style={{
-                        position: 'absolute',
-                        left: '0',
-                        top: '0',
-                        width: '100%',
-                        height: '100%',
-                        zIndex: '-1'
-                    }}
+            {!isLoaded ?
+                <motion.div className={s.loader__wrapper}
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    exit={{ opacity: 0, scale: 2 }}
                 >
-                    {`
+                    <div className={s.circle}>
+                        <div className={s.mask}></div>
+                        <div className={s.dot}></div>
+                    </div>
+                    <div className={s.loader__progress}>
+                        <CountUp end={progress} />%
+                    </div>
+                    <div className={activeBtn ? s.btn + ' ' + s.active : s.btn} onClick={() => setIsLoaded(true)}>
+                        <UI_Button text={'To Explore'} arrow />
+                    </div>
+                    <css-doodle
+                        style={{
+                            position: 'absolute',
+                            left: '0',
+                            top: '0',
+                            width: '100%',
+                            height: '100%',
+                            zIndex: '-1'
+                        }}
+                    >
+                        {`
                     :doodle {
                         @grid: 10x1 / 100vw 100vh / transparent;
                         --color: #E4E4E7, #A3A4AB, #E4E4E7, #4B4D5A, #E4E4E7, #FEC532, #E4E4E7;
-                        --color2:#E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #FEC532;
+                        --color2:#E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #E4E4E7, #FEC532; 
                     }
 
                     :after, :before {
@@ -89,18 +90,17 @@ export default function Loader() {
                     @keyframes move-around {
                         0%, 100% {
                             transform: translate(0, 0);
-                            opacity: 0.8;
+                            opacity: 0.6;
                         }
                         50% {
                             transform: translate(@r(-100%, 100%), @r(-100%, 100%)); 
-                            opacity: 0.8;
+                            opacity: 0.6;
                         }
                     }
                 `}
-                </css-doodle>
-            </motion.div> :
-                null
-            }
+                    </css-doodle>
+                </motion.div>
+                : null}
         </AnimatePresence>
     );
 }
