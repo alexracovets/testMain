d8f58b0d7b81b3ac41b9723c53a6c2627eb8f1dd
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-14 15:09:28 +0300

    fixed loader

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
           {!isMobile ? <VoxelModel /> : null}
         </Suspense>
         <Suspense fallback={null}>
-          {isMobile ? <MobileModel /> : null}
+          {isMobile && userInteracted? <MobileModel /> : null}
         </Suspense>
         <Suspense fallback={null}>
           <Pager />
