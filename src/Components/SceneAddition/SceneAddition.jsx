3e54bf9e4e222fbfd7be90afc3adc6a08c2badd9
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-18 13:46:33 +0300

    fixed bg

--- src/Components/SceneAddition/SceneAddition.jsx
+++ src/Components/SceneAddition/SceneAddition.jsx
@@ -8,7 +8,7 @@ export default function SceneAddition() {
 
     return (
         <>
-            <color attach={'background'} args={['#292929']} />
+            <color attach={'background'} args={['#08080a']} />
             <pointLight position={[-30, 0, -30]} power={10.1} />
             <ambientLight />
             <directionalLight />
