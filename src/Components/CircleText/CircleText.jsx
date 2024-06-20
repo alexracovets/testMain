87144ef87846292e0ba6412c3147004518c1abf1
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-20 09:56:37 +0300

    test

--- src/Components/CircleText/CircleText.jsx
+++ src/Components/CircleText/CircleText.jsx
@@ -4,10 +4,11 @@ import 'splitting/dist/splitting.css';
 import './CircleText.scss';
 
 export default function CircleText() {
-    const circleRef = useRef(null);
+    const circleRef1 = useRef(null);
     const circleRef2 = useRef(null);
-    const circleRef3 = useRef(null); 
-    
+    const circleRef3 = useRef(null);
+    const circleRef4 = useRef(null);
+
     const [isInitial, setIsInitial] = useState(false);
     useEffect(() => {
         Splitting();
@@ -32,24 +33,36 @@ export default function CircleText() {
 
     useEffect(() => {
         if (isInitial) {
-            initial(circleRef.current)
+            initial(circleRef1.current)
             initial(circleRef2.current)
-            initial(circleRef3.current) 
+            initial(circleRef3.current)
+            initial(circleRef4.current)
         }
 
     }, [isInitial]);
     return (
         <>
-
-            <div ref={circleRef} className="circle" data-splitting>
-                DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
-            </div>
-
-            <div ref={circleRef2} className="circle test" data-splitting>
-                DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
-            </div>
-            <div ref={circleRef3} className="circle test2" data-splitting>
-                DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
+            <div className='wrapper'>
+                <div className='wrapper__circle'>
+                    <div ref={circleRef1} className="circle" data-splitting>
+                        DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
+                    </div>
+                </div>
+                <div className='wrapper__circle'>
+                    <div ref={circleRef2} className="circle" data-splitting>
+                        DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
+                    </div>
+                </div>
+                <div className='wrapper__circle reverse'>
+                    <div ref={circleRef3} className="circle" data-splitting>
+                        DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
+                    </div>
+                </div>
+                <div className='wrapper__circle reverse'>
+                    <div ref={circleRef4} className="circle" data-splitting>
+                        DIGITAL&nbsp;&nbsp;PRODUCTION&nbsp;&nbsp;STUDIO&nbsp;&nbsp;
+                    </div>
+                </div>
             </div> 
         </>
     );
