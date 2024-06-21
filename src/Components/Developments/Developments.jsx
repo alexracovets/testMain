b34bbe9f6a7a3222957dbaf61486306ee007d9c2
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-21 11:51:12 +0300

    fixed list

--- src/Components/Developments/Developments.jsx
+++ src/Components/Developments/Developments.jsx
@@ -2,73 +2,47 @@ import ai from '/image/icons/developments/ai.svg';
 import xr from '/image/icons/developments/xr.svg';
 import dev from '/image/icons/developments/dev.svg';
 import digital from '/image/icons/developments/digital.svg';
-
+import Ticker from 'react-ticker'
+import TextLoop from "react-text-loop";
+import Marquee from "react-fast-marquee";
 import s from './Developments.module.scss';
 
 export default function Developments() {
     return (
         <div className={s.developments__wrapper}>
             <div className={s.developments}>
-                <ul className={s.developments__list}>
-                    <li>
-                        <img src={ai} alt='Artificial Intelligence' />
-                        <span className={s.text}>
-                            <span>Artificial</span>
-                            <span>Intelligence</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={xr} alt='Extended Reality' />
-                        <span className={s.text}>
-                            <span>Extended</span>
-                            <span>Reality</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={dev} alt='Web Development' />
-                        <span className={s.text}>
-                            <span>Web</span>
-                            <span>Development</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={digital} alt='Digital Marketing' />
-                        <span className={s.text}>
-                            <span>Digital</span>
-                            <span>Marketing</span>
-                        </span>
-                    </li>
-                </ul>
-                <ul className={s.developments__list}>
-                    <li>
-                        <img src={ai} alt='Artificial Intelligence' />
-                        <span className={s.text}>
-                            <span>Artificial</span>
-                            <span>Intelligence</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={xr} alt='Extended Reality' />
-                        <span className={s.text}>
-                            <span>Extended</span>
-                            <span>Reality</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={dev} alt='Web Development' />
-                        <span className={s.text}>
-                            <span>Web</span>
-                            <span>Development</span>
-                        </span>
-                    </li>
-                    <li>
-                        <img src={digital} alt='Digital Marketing' />
-                        <span className={s.text}>
-                            <span>Digital</span>
-                            <span>Marketing</span>
-                        </span>
-                    </li>
-                </ul>
+                <Marquee autoFill>
+                    <ul className={s.developments__list}>
+                        <li>
+                            <img src={ai} alt='Artificial Intelligence' />
+                            <span className={s.text}>
+                                <span>Artificial</span>
+                                <span>Intelligence</span>
+                            </span>
+                        </li>
+                        <li>
+                            <img src={xr} alt='Extended Reality' />
+                            <span className={s.text}>
+                                <span>Extended</span>
+                                <span>Reality</span>
+                            </span>
+                        </li>
+                        <li>
+                            <img src={dev} alt='Web Development' />
+                            <span className={s.text}>
+                                <span>Web</span>
+                                <span>Development</span>
+                            </span>
+                        </li>
+                        <li>
+                            <img src={digital} alt='Digital Marketing' />
+                            <span className={s.text}>
+                                <span>Digital</span>
+                                <span>Marketing</span>
+                            </span>
+                        </li>
+                    </ul> 
+                </Marquee>
             </div>
         </div>
     );
