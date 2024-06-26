db60ea75b5c0b3d7713dc4818d33551957df5029
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-26 10:14:31 +0300

    fixed style

--- src/Components/TextField/TextField.jsx
+++ src/Components/TextField/TextField.jsx
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 
-import s from './TextField.module.scss';
-
 export default function TextField({ texts }) {
     return (
         <>
             {texts.map((item, idx) => {
                 if (item.type === 'textList') {
-                    return <div className={s.text_list} key={idx}>
+                    return <div key={idx}>
                         {item.value.map((element, index) => <p key={index} dangerouslySetInnerHTML={{ __html: element }} />)}
                     </div>
                 } else if (item.type === 'text') {
                     return <p key={idx} dangerouslySetInnerHTML={{ __html: item.value }} />
                 } else if (item.type === 'list') {
-                    return <ul key={idx} className={s.list}>
+                    return <ul key={idx}  >
                         {item.list.map((li, index) => <li key={index}> <p key={idx} dangerouslySetInnerHTML={{ __html: li }} /></li>)}
                     </ul>
                 }
