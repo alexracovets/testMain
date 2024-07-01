c965a120463b8cb740c563c17bde89e9ea027733
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-07-01 11:21:51 +0300

    fixed style

--- src/Components/Modal/ContactsForm/ContactsForm.jsx
+++ src/Components/Modal/ContactsForm/ContactsForm.jsx
@@ -60,8 +60,62 @@ export default function ContactsForm() {
     }, [userForm]);
 
     return (
-        <AnimatePresence>
-            {isActive ?
+        <>
+            <AnimatePresence>
+                {isActive ?
+                    <motion.div className={s.modal}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.3 }}
+                        exit={{ opacity: 0 }}
+                        onClick={() => setIsActiveForm(false)}
+                    >
+                        <div className={s.form__wrapper} onClick={(e) => e.stopPropagation()}>
+                            <div className={s.cross} onClick={() => setIsActiveForm(false)}>
+                                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M19.001 5.28296L5.00098 19.283" stroke="#A3A4AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M5.00098 5.28296L19.001 19.283" stroke="#A3A4AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+
+                            </div>
+                            <div className={s.title}>
+                                Contact Us
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <ContactsFormInput
+                                    name={`full_name`}
+                                    label={`Full name`}
+                                    placeholder={`John Doe`}
+                                    type={`text`}
+                                    setUserForm={setUserForm}
+                                    userForm={userForm}
+                                />
+                                <ContactsFormInput
+                                    name={`email`}
+                                    label={`Email address`}
+                                    placeholder={`Enter your email`}
+                                    type={`email`}
+                                    setUserForm={setUserForm}
+                                    userForm={userForm}
+                                />
+                                <ContactsFormTextArea
+                                    name={`textarea`}
+                                    label={`Comment`}
+                                    placeholder={`Add your comment or short project description`}
+                                    type={`textarea`}
+                                    setUserForm={setUserForm}
+                                    userForm={userForm}
+                                />
+                                <div className={s.btn} type="submit">
+                                    <UI_Button text={'Contact me'} arrow submit small disabled={!isBtnActive} />
+                                </div>
+                            </form>
+                        </div>
+                    </motion.div> :
+                    null
+                }
+            </AnimatePresence >
+            {/* <AnimatePresence>
                 <motion.div className={s.modal}
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
@@ -69,50 +123,9 @@ export default function ContactsForm() {
                     exit={{ opacity: 0 }}
                     onClick={() => setIsActiveForm(false)}
                 >
-                    <div className={s.form__wrapper} onClick={(e) => e.stopPropagation()}>
-                        <div className={s.cross} onClick={() => setIsActiveForm(false)}>
-                            <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M19.001 5.28296L5.00098 19.283" stroke="#A3A4AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d="M5.00098 5.28296L19.001 19.283" stroke="#A3A4AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-
-                        </div>
-                        <div className={s.title}>
-                            Contact Us
-                        </div>
-                        <form onSubmit={handleSubmit}>
-                            <ContactsFormInput
-                                name={`full_name`}
-                                label={`Full name`}
-                                placeholder={`John Doe`}
-                                type={`text`}
-                                setUserForm={setUserForm}
-                                userForm={userForm}
-                            />
-                            <ContactsFormInput
-                                name={`email`}
-                                label={`Email address`}
-                                placeholder={`Enter your email`}
-                                type={`email`}
-                                setUserForm={setUserForm}
-                                userForm={userForm}
-                            />
-                            <ContactsFormTextArea
-                                name={`textarea`}
-                                label={`Comment`}
-                                placeholder={`Add your comment or short project description`}
-                                type={`textarea`}
-                                setUserForm={setUserForm}
-                                userForm={userForm}
-                            />
-                            <div className={s.btn} type="submit">
-                                <UI_Button text={'Contact me'} arrow submit small disabled={!isBtnActive} />
-                            </div>
-                        </form>
-                    </div>
-                </motion.div> :
-                null
-            }
-        </AnimatePresence >
+                  
+                </motion.div>
+            </AnimatePresence > */}
+        </>
     );
 }  
