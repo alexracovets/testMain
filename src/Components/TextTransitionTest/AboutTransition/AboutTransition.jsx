df6fc350d15c71520563f08136f7f3f24b3229e4
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-07-22 13:49:36 +0300

    fixed style

--- src/Components/TextTransitionTest/AboutTransition/AboutTransition.jsx
+++ src/Components/TextTransitionTest/AboutTransition/AboutTransition.jsx
@@ -1,26 +1,56 @@
-import TextTransition from 'react-text-transition';
+// import TextTransition from 'react-text-transition';
+// import { useEffect, useState } from 'react';
+
+// import useLoader from '../../../store/useLoader';
+
+// const TEXTS = [`00`, `11`, `22`, `33`, '32', '31', '30'];
+
+// import s from '../TextTransitionTest.module.scss';
+// export default function AboutTransition() {
+//     const [index, setIndex] = useState(0);
+//     const isLoaded = useLoader((state) => state.isLoaded);
+
+//     useEffect(() => {
+//         if (index !== TEXTS.length - 1 && isLoaded) {
+//             setTimeout(() => {
+//                 setIndex(prev => prev + 1)
+//             }, 200)
+//         }
+//     }, [index, isLoaded])
+
+//     return (
+//         <>
+//             We launched <TextTransition inline className={s.transition}>{TEXTS[index]}</TextTransition> <span className={s.plus}></span> projects
+//         </>
+//     );
+// }
 import { useEffect, useState } from 'react';
-
 import useLoader from '../../../store/useLoader';
-
-const TEXTS = [`00`, `11`, `22`, `33`, '32', '31', '30'];
+import gsap from 'gsap';
 
 import s from '../TextTransitionTest.module.scss';
 export default function AboutTransition() {
-    const [index, setIndex] = useState(0);
+    const [count, setCount] = useState(0);
     const isLoaded = useLoader((state) => state.isLoaded);
 
     useEffect(() => {
-        if (index !== TEXTS.length - 1 && isLoaded) {
+        const countObj = { value: 0 };
+        if (isLoaded) {
             setTimeout(() => {
-                setIndex(prev => prev + 1)
-            }, 200)
+                gsap.to(countObj, {
+                    value: 30,
+                    duration: 3,
+                    ease: "power1.out",
+                    onUpdate: () => {
+                        setCount(countObj.value.toFixed(0)); // Оновлюємо стан на кожен кадр
+                    }
+                });
+            }, 500)
         }
-    }, [index, isLoaded])
+
+    }, [isLoaded]);
 
     return (
-        <>
-            We launched <TextTransition inline className={s.transition}>{TEXTS[index]}</TextTransition> <span className={s.plus}></span> projects
-        </>
+        <p>We launched <span className={s.count}> {count < 10 ? '0' + count : count}</span> <span className={s.plus}></span> projects</p>
     );
 }
